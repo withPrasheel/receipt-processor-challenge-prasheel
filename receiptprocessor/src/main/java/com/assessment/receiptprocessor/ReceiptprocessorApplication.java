85e4f9834bc44b74d1b11ca6eb8fb0ca35d6c297
Prasheel <prasheelnandwana3@gmail.com>
2024-11-15 21:50:04 -0700

    adding git ignore and sprint project initilisation

---
+++ receiptprocessor/src/main/java/com/assessment/receiptprocessor/ReceiptprocessorApplication.java
@@ -0,0 +1,13 @@
+package com.assessment.receiptprocessor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReceiptprocessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ReceiptprocessorApplication.class, args);
+	}
+
+}
---
+++ receiptprocessor/src/test/java/com/assessment/receiptprocessor/ReceiptprocessorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.assessment.receiptprocessor;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReceiptprocessorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
